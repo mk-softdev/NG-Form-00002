ccc87b86f963ac4a6a66d6755e0ba562fa7d4428
‘Manas <manasp.softdev@gmail.com>
2021-09-04 19:15:31 +0530

    'template-driven-form-updated'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,30 @@ export class AppComponent {
 
   topics = ['Angular','React','Vue'];
 
-  userModel = new User('Manas','manaspradhan589@gmail.com',8800363523,'','morning',true);
+  topicHasError = true;
+
+  userModel = new User('Manas','manaspradhan589@gmail.com',8800363523,'default','morning',true);
+
+  constructor(private _enrollmentService:EnrollmentService){
+ 
+  }
+
+  validateTopic(value:any){
+    if(value==='default'){
+      this.topicHasError = true;
+    }
+    else{
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit(){
+    //console.log(this.userModel);
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log('Success!', data),
+      error => console.error('Error!', error)
+    )
+  }
 
 }
